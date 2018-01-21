73b22e348d88b512cd9b68e829345918b0c9d0dd
svladimirov <ivc.vladimirovsa@gmail.com>
2018-01-21 21:38:49 +1000

    global urls

--- app1/urls.py
+++ app1/urls.py
@@ -17,8 +17,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.artist_list, name='Home'),
+    path('', views.artist_list, name='artist_list'),
     path('artist/<str:artist_name>/', views.tracks_list, name='artist'),
+    path('album/', views.album_list, name='album_list'),
+    path('album/<str:album_name>/', views.album_list_track, name='album'),
     path('genre/', views.genge_list, name='genre_list'),
     path('genre/<str:genre_name>/', views.genre_list_track, name='genre'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from django.http import Http404
-from .models import Artist, Genre
+from .models import Artist, Genre, Album
 from .filters import ArtistFilter, GenreFilter
 
 def artist_list(request):
     artist_list = Artist.objects.all()
     artist_filter = ArtistFilter(request.GET, queryset=artist_list)
-    return render(request, 'app1/index.html', {'filter': artist_filter})
+    return render(request, 'app1/artist.html', {'filter': artist_filter})
 
 def tracks_list(request, artist_name):
     try:
         artist = Artist.objects.get(name=artist_name)
+        albums = Album.objects.filter(tracks_of_album__artist=artist).values('name').distinct()
     except Artist.DoesNotExist:
         raise Http404
     return render(request, 'app1/tracks.html', {
         'tracks': artist.tracks_of_artist.select_related(),
         'artist': artist,
+        'albums': albums,
+
     })
 def genre_list_track(request, genre_name):
     try:
@@ -30,3 +33,17 @@ def genge_list(request):
     genre = Genre.objects.all()
     genre_filter = GenreFilter(request.GET, queryset=genre)
     return render(request, 'app1/genre_list.html', {'filter': genre_filter})
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'app1/album_list.html', {'albums': albums})
+
+def album_list_track(request, album_name):
+    try:
+        album = Album.objects.get(name=album_name)
+    except Genre.DoesNotExist:
+        raise Http404
+    return  render(request, 'app1/album.html', {
+        'album': album,
+        'tracks': album.tracks_of_album.select_related()
+    })
